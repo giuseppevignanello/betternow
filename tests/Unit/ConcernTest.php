bb499d0bbd0aec8af25e83d95e742f7140aa74ce
giuseppevignanello <peppe.vignanello@gmail.com>
2024-11-02 10:49:10 +0100

    Fix Test and Seed

--- tests/Unit/ConcernTest.php
+++ tests/Unit/ConcernTest.php
@@ -3,19 +3,17 @@
 namespace Tests\Unit;
 
 use App\Models\Concern\Concern;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase;
 
 class ConcernTest extends TestCase
 {
-    use RefreshDatabase;
     public function test_it_can_create_a_concern()
     {
         $data = [
             'title' => 'Anxiety Test',
             'text' => null,
             'expected_resolution_date' => null,
-            'risk_level' => config('general.riskLevelIds.high'),
+            'risk_level_id' => config('general.riskLevelIds.high'),
             'status_id' => config('general.statusIds.open'),
         ];
 
@@ -24,7 +22,7 @@ class ConcernTest extends TestCase
         $this->assertDatabaseHas('concerns', $data);
 
         $highRiskLevelId = 3;
-        $this->assertEquals($highRiskLevelId, $concern->risk_level);
+        $this->assertEquals($highRiskLevelId, $concern->risk_level_id);
 
         $openStatusId = 1;
         $this->assertEquals($openStatusId, $concern->status_id);
